daca0171cfaca980e15ca1d2d32433eeb58d265c
Akash Raina <kevinraina279@gmail.com>
2026-09-04 12:15:02 +0530

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.cpp
@@ -0,0 +1,41 @@
+class MyHashSet {
+    
+
+public:
+
+    vector<int> hashSet;
+    MyHashSet() {
+
+    }
+    
+    void add(int key) {
+        bool isPresent = contains(key);
+
+        if(!isPresent) hashSet.push_back(key);
+    }
+    
+    void remove(int key) {
+
+        for(int i = 0; i < hashSet.size(); i++){
+            if(hashSet[i] == key){
+                hashSet.erase(hashSet.begin()  + i);
+                return;
+            } 
+        };
+    }
+    
+    bool contains(int key) {
+        for(auto value : hashSet){
+            if(value == key) return true;
+        };
+        return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
